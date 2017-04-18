72646453efafdf662ce6a7baa64290327a99b9a6
Giulio Bernardi <ugilio@gmail.com>
2017-04-18 14:20:47 +0200

    Ddl Nature: added copyright headers

--- it.cnr.istc.keen.ui/src/it/cnr/istc/keen/ui/nature/SilentNatureAddingEditorCallback.java
+++ it.cnr.istc.keen.ui/src/it/cnr/istc/keen/ui/nature/SilentNatureAddingEditorCallback.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2016-2017 PST (http://istc.cnr.it/group/pst).
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Giulio Bernardi
+ */
 package it.cnr.istc.keen.ui.nature;
 
 import org.apache.log4j.Logger;
